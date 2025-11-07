a44d1b954b528b7c071e7730a8004d8379a01529
A3Alumno14 <javiru01ja@gmail.com>
2025-11-07 12:59:27 +0100

    Entrega Jaime Tarea 2

---
+++ Jaime_Ruiz/src/ControlNotas.java
@@ -0,0 +1,32 @@
+public class ControlNotas {
+
+    //Inicializamos el metodo MyScanner
+    private static final MyScanner sc = new MyScanner();
+
+    public static void main(String[] args) {
+        System.out.println("=== Control de Notas ===");
+        //Introduce los datos
+        String nombre = sc.pedirSoloTexto("Introduce el nombre del alumno: ");
+
+        double nota1= sc.pedirDecimal("Introduce la nota de base de datos: ");
+        double nota2= sc.pedirDecimal("Introduce la nota de programación: ");
+        double nota3=sc.pedirDecimal("Introduce la nota de base de entornos: ");
+
+
+
+        //Creamos objeto alumno
+        Alumno alumno = new Alumno(nombre, nota1, nota2, nota3);
+
+
+        //Llamamos al metodo MostrarResumen
+        alumno.MostrarResumen();
+
+
+        //Cerramos el MyScanner
+        sc.cerrar();
+
+    }
+
+
+
+}
