b1a0725033a1bb7088bc277d68fd5038de67e8f1
Julio Martin Rodriguez Sanchez <julitors@gmail.com>
2025-11-07 13:08:46 +0100

    JulioRodriguez DAW 1 Año

---
+++ JulioRodriguez/src/Simulacros/MainEmpleado.java
@@ -0,0 +1,20 @@
+package Simulacros;
+
+public class MainEmpleado {
+    public static void main(String[] args) {
+        MyScanner MySc = new MyScanner();
+
+        System.out.println("=== GESTOR DE EMPLEADOS ===");
+
+        String nombre = MySc.pedirTexto("Introduce el nombre del empleado: ");
+        String cargo = MySc.pedirTexto("Introduce el cargo: ");
+        double sueldo = MySc.pedirDecimal("Introduce el sueldo base: ");
+        double bono = MySc.pedirDecimal("Introduce el porcentaje de bonificación: ");
+        double irpf = MySc.pedirDecimal("Introduce el porcentaje de IRPF: ");
+
+        Empleado e = new Empleado(nombre, cargo, sueldo, bono, irpf);
+        e.mostrarResum();
+
+        MySc.cerrar();
+    }
+}
